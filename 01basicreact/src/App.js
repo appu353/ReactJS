29ac3aff107d9a22aa3a0bf54f61a4dad8ab534e
Aparna <Aparna.Kumari@minebea-intec.com>
2023-12-04 18:04:46 +0530

    Deleted few files

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      Code in React | create-react-app:-is utility or basically software. But
+      this utility is bulky and it will take time to create project. It will
+      install unnecessary things to our system. Command for creating project
+      is:- npx create-react-app project name.(node js should be installed) To
+      run project:-npm run start or npm start. Local host port number is 3000.
+      npm run build:- command to run in production. Build folder will be created
+      and code will get converted into JS. Build folder will be served to the
+      user in production, not src folder. Creating project with create-react-app
+      is bulky way of creating project. So we will use Vite to create project.
+      Create react project using Vite. Vite is bundler.
+    </h1>
   );
 }
 
