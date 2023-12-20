4d90ee10a6e439d2d270c192b50225b6d9427129
Aparna <Aparna.Kumari@minebea-intec.com>
2023-12-20 17:56:39 +0530

    Production grade react components

--- 12MegaBlog/src/components/Logo.jsx
+++ 12MegaBlog/src/components/Logo.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Logo({ width = "100px" }) {
-  return (
-    <>
-      <div>Logo</div>
-    </>
-  );
+  return <div>Logo</div>;
 }
 
 export default Logo;
--- 12MegaBlog/src/components/container/Container.jsx
+++ 12MegaBlog/src/components/container/Container.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 
 function Container({ children }) {
-  return (
-    <>
-      <div className="w-full max-w-7xl mx-auto px-4">{children}</div>
-    </>
-  );
+  return <div className="w-full max-w-7xl mx-auto px-4">{children}</div>;
 }
 
 export default Container;
+//If return has only one statement, no need to use bracket after return, make sure that we are using  semicolon at the end of statement.
